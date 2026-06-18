3a4c9c9e5c6d93ba05a221c7076f3053ea0a2359
hoangkhang <hoangkhang282006@gmail.com>
2026-06-19 01:32:25 +0700

    in process to fix User class

---
+++ src/main/java/com/example/IpadViet/Entity/Role.java
@@ -0,0 +1,5 @@
+package com.example.IpadViet.Entity;
+
+public enum Role {
+    CUSTOMER, ADMIN
+}
--- src/main/java/com/example/IpadViet/Entity/User.java
+++ src/main/java/com/example/IpadViet/Entity/User.java
@@ -1,4 +1,59 @@
 package com.example.IpadViet.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.CUSTOMER;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now()
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
+
 }
